2539af8be559b415e41299c33fe9a6144821fb9f
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-10-31 10:14:27 -0500

    Initial Commit

---
+++ iTour SwiftData Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour SwiftData Demo
+//
+//  Created by Randy McKown on 10/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTour SwiftData Demo/iTour_SwiftData_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_SwiftData_DemoApp.swift
+//  iTour SwiftData Demo
+//
+//  Created by Randy McKown on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct iTour_SwiftData_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
