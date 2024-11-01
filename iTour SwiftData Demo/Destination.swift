cc5b0b65ad7036db3523b6a2804192569cad3cb2
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-11-01 09:22:03 -0500

    SwiftData Setup

---
+++ iTour SwiftData Demo/Destination.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  iTour SwiftData Demo
+//
+//  Created by Randy McKown on 10/31/24.
+//
+
---
+++ iTour SwiftData Demo/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour SwiftData Demo
+//
+//  Created by Randy McKown on 10/31/24.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
