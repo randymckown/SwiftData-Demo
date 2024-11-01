da85a62b868206809640e8374e0de4b1319e7404
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-11-01 10:43:21 -0500

    Added Sorting

---
+++ iTour SwiftData Demo/DestinationListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationListingView.swift
+//  iTour SwiftData Demo
+//
+//  Created by Randy McKown on 11/1/24.
+//
+
+import SwiftUI
+
+struct DestinationListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
